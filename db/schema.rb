8de179d3d821213e37a7d7916b2f3590bd1cb274
XiaoA <abuckingham99@gmail.com>
2015-06-30 06:43:39 -0500

    Migrate Videos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630113746) do
+ActiveRecord::Schema.define(version: 20150630113924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "videos", force: true do |t|
+    t.string   "title"
+    t.string   "small_cover_url"
+    t.string   "large_cover_url"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
