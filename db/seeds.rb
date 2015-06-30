3d52d246b7d09e881f40712bd222f44ab592eb4e
XiaoA <abuckingham99@gmail.com>
2015-06-30 06:41:33 -0500

    Correct syntax errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Video.create(title: 'Family Guy',
-             description: "You'll love spending time with Peter Griffin and his oddball family."
-             small_cover_url: 'family_guy.jpg',
-             large_cover_url: 'family_guy.jpg')
+Video.create(title: "Family Guy",
+             description: "You'll love spending time with Peter Griffin and his oddball family.",
+             small_cover_url: "/public/tmp/family_guy.jpg",
+             large_cover_url: "/public/tmp/family_guy.jpg")
 
-Video.create(title: "Futurama"
+Video.create(title: "Futurama",
              description: "A pizza delivery boy, cryogenically preserved for 1,000 years, adjusts to his new life in the 31st Century",
-             small_cover_url: "futurama.jpg", 
-             large_cover_url: "futurama.jpg")
+             small_cover_url: "/public/tmp/futurama.jpg", 
+             large_cover_url: "/public/tmp/futurama.jpg")
 
-Video.create(title: "Monk"
+Video.create(title: "Monk",
              description: "A brilliant detective, riddled with OCD and phobias, solves crimes in San Francisco, while searching for his wife's elusive killer.",
-             small_cover_url: "monk.jpg",     
-             large_cover_url: "monk.jpg")
+             small_cover_url: "/public/tmp/monk.jpg",     
+             large_cover_url: "/public/tmp/monk.jpg")
 
-Video.create(title: "South Park"
+Video.create(title: "South Park",
              description: "Nothing is sacred in South Park. Join Stan, Kyle, Cartman, Kenny, and the rest of the town for adventures that are both delightful and distubring.",
-             small_cover_url: "south_park.jpg",
-             large_cover_url: "south_park.jpg")
+             small_cover_url: "/public/tmp/south_park.jpg",
+             large_cover_url: "/public/tmp/south_park.jpg")
