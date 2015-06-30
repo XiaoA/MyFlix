41aed7bfa3f2c309ebdf3a8bf0b2f6ab4ce998d4
XiaoA <abuckingham99@gmail.com>
2015-06-30 06:42:39 -0500

    Create Videos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629192927) do
+ActiveRecord::Schema.define(version: 20150630113746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: true do |t|
-    t.string   "title"
-    t.string   "small_cover_url"
-    t.string   "large_cover_url"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
