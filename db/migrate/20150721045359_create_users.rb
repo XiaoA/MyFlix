b4b00f7592593ae3de64b663f44e81ba0b804456
XiaoA <abuckingham99@gmail.com>
2015-07-21 00:16:50 -0500

    Set up @user object, model, and 'new' view

--- db/migrate/20150721045359_create_users.rb
+++ db/migrate/20150721045359_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :title, :small_cover_url, :large_cover_url
-      t.text :description
+      t.string :email
+      t.string :password_digest
+      t.string :full_name
       t.timestamps
     end
   end
