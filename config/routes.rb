39d4e654b7462770b8f18e2f5c3ec647c939fe2f
XiaoA <abuckingham99@gmail.com>
2015-07-22 15:22:11 -0500

    Implement user sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Myflix::Application.routes.draw do
   end
   
   resources :categories, only: :show
-
+  resources :users
+  
   get 'ui(/:action)', controller: 'ui'
   get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
 
-  resources :users
+
 end
