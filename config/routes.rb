247286b0ee5759b7bb723614c36c5f62532e2b23
XiaoA <abuckingham99@gmail.com>
2015-07-23 23:13:23 -0500

    Add post method for 'sign_in' route

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
-
+  get 'home', to: "videos#index"
+  get 'ui(/:action)', controller: 'ui'
+  get 'register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  
   resources :videos, only: :show do
     collection do
       get :search, to: 'videos#search'
     end
   end
-  
+    
   resources :categories, only: :show
   resources :users, only: :create
   resources :sessions, only: :create
-  
-  
-  get 'ui(/:action)', controller: 'ui'
-  get 'register', to: 'users#new'
-  get 'sign_in', to: 'sessions#new'
-
 
 end
