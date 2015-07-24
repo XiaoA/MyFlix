439da6f9befa455ce6cbf45486240cc72f8c33a9
XiaoA <abuckingham99@gmail.com>
2015-07-23 23:13:56 -0500

    Fix 'create' action for sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome. You've logged in!"
+      redirect_to home_path
+    else
+      flash[:danger] = "There was a problem with your username or password."
+      render :new
+    end
+  end
+
 end
