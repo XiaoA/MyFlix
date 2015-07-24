f87371b6a604268cac8a1a970801fb6128fa5201
XiaoA <abuckingham99@gmail.com>
2015-07-23 23:44:43 -0500

    Add user 'sign_out'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You've successfully logged out."
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Myflix::Application.routes.draw do
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
-  
+  get 'sign_out', to: 'sessions#destroy'
+
   resources :videos, only: :show do
     collection do
       get :search, to: 'videos#search'
