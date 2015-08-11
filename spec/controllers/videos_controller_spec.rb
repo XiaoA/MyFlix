55115787387f438dd1e42c8ff0bc83099c4871a7
XiaoA <abuckingham99@gmail.com>
2015-08-10 22:19:28 -0700

    Refactor videos#show Rspec tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-    context "with authenticated users" do
-      before do
-        session[:user_id] = Fabricate(:user).id
-      end
-      
-      it "sets @video" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(assigns(:video)).to eq(video)
-      end 
+    it "sets @video for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
     end
 
+    it "redirects the user to the sign in page for unauthenticated users" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
   end
 end
 
