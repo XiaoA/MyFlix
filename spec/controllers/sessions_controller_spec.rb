c7dd89acfc969f6c918dbcc292c93ee82639affd
XiaoA <abuckingham99@gmail.com>
2015-08-14 19:52:20 -0700

    Refactor sessions_controller_spec.rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe SessionsController do
   describe "GET new" do
-    it "render the new template for unauthenticated users" do
+    it "renders the new template for unauthenticated users" do
       get :new
       expect(response).to render_template :new
     end
-
+    
     it "redirects to the home page for authenticated users" do
       session[:user_id] = Fabricate(:user).id
       get :new
@@ -16,40 +16,43 @@ describe SessionsController do
 
   describe "POST create" do
     context "with valid credentials" do
-      before do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password
-      end
-      
+      let(:user) { Fabricate(:user) }
+          
       it "puts the signed-in user in the session" do
-        expect(session[:user_id]).to eq(andrew.id)
+        post :create, email: user.email, password: user.password
+        expect(session[:user_id]).to eq(user.id)
       end
 
       it "redirects to the home page" do
+        post :create, email: user.email, password: user.password
         expect(response).to redirect_to home_path
       end
 
       it "sets the notice" do
+        post :create, email: user.email, password: user.password
         expect(flash[:success]).not_to be_blank
       end
     end
 
     context "with invalid credentials" do
+      let(:user) { Fabricate(:user) }
       before do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password + 'asdfghjkl'        
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
       end
 
       it "does not create a new session for the user" do
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         expect(session[:user_id]).to be_nil
       end
 
       it "redirects to the sign_in page" do
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         get :new
         expect(response).to render_template :new
       end
 
       it "sets the error message" do
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         expect(flash[:danger]).not_to be_blank
       end
     end
