124555d738e3e5ac8cc40032a4558fb5fc2b8d2e
XiaoA <abuckingham99@gmail.com>
2015-11-20 13:56:02 -0800

    Add sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe SessionsController do
   describe "GET new" do
-    it "render the new template for unauthenticated users" do
+    it "renders the new template for unauthenticated users" do
       get :new
       expect(response).to render_template :new
     end
-
+    
     it "redirects to the home page for authenticated users" do
       session[:user_id] = Fabricate(:user).id
       get :new
@@ -16,63 +16,63 @@ describe SessionsController do
 
   describe "POST create" do
     context "with valid credentials" do
+      let(:user) { Fabricate(:user) }
+          
       it "puts the signed-in user in the session" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password
-        expect(session[:user_id]).to eq(andrew.id)
+        post :create, email: user.email, password: user.password
+        expect(session[:user_id]).to eq(user.id)
       end
 
       it "redirects to the home page" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password
+        post :create, email: user.email, password: user.password
         expect(response).to redirect_to home_path
       end
 
       it "sets the notice" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password
+        post :create, email: user.email, password: user.password
         expect(flash[:success]).not_to be_blank
       end
     end
 
     context "with invalid credentials" do
+      let(:user) { Fabricate(:user) }
+      before do
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
+      end
+
       it "does not create a new session for the user" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password + 'asdfghjkl'
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         expect(session[:user_id]).to be_nil
       end
 
       it "redirects to the sign_in page" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password + 'asdfghjkl'
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         get :new
         expect(response).to render_template :new
       end
 
       it "sets the error message" do
-        andrew = Fabricate(:user)
-        post :create, email: andrew.email, password: andrew.password + 'asdfghjkl'
+        post :create, email: user.email, password: user.password + 'asdfghjkl'        
         expect(flash[:danger]).not_to be_blank
       end
     end
   end
 
   describe "GET destroy" do
-    it "clears out the session for the user" do
+    before do
       session[:user_id] = Fabricate(:user).id
-      get :destroy
+      get :destroy      
+    end
+
+    it "clears out the session for the user" do
       expect(session[:user_id]).to be_nil
     end
 
     it "redirects to the root path" do
-      session[:user_id] = Fabricate(:user).id
-      get :destroy
       expect(response).to redirect_to root_path
     end
 
     it "set the notice" do
-      session[:user_id] = Fabricate(:user).id
-      get :destroy
       expect(flash[:success]).not_to be_nil
     end
   end
