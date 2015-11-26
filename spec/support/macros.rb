58422aa75511eb093046a314a3351ff73bd6bfe5
XiaoA <abuckingham99@gmail.com>
2015-11-25 23:50:22 -0800

    Add macros/shared examples, queue items controller

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,13 +1,5 @@
 #spec/support/macros.rb
-def set_current_user
-  andrew = Fabricate(:user)
-  session[:user_id] = andrew.id
+def set_current_user(user=nil)
+  session[:user_id] = (user || Fabricate(:user)).id
 end
 
-def curent_user
-  User.find(session[:user_id])
-end
-
-def clear_current_user
-  session[:user_id] = nil
-end
